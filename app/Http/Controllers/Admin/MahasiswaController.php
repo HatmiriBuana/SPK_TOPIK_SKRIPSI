5640b4e1f475e0c4b53410dcf210344165a8e550
HatmiriBuana <hatmirib@gmail.com>
2024-03-18 18:37:11 +0700

    memperbaiki table mahasiswa yang gabung jadi table user

--- app/Http/Controllers/Admin/MahasiswaController.php
+++ app/Http/Controllers/Admin/MahasiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Mahasiswa;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,7 +12,7 @@ class MahasiswaController extends Controller
 {
     public function index()
     {
-        $students = DB::table('mahasiswas')->select('id', 'nama', 'nim', 'prodi', 'fakultas')->get(); 
+        $students = DB::table('users')->where('role_id', 1)->select('id', 'name', 'nim', 'study_program', 'major')->get(); 
         return view('admin.mahasiswa.index', ['students' => $students]);
     }
 
@@ -23,7 +24,7 @@ class MahasiswaController extends Controller
     public function edit($id)
     {
         try{
-            $student = Mahasiswa::findOrFail($id);
+            $student = User::findOrFail($id);
             return view('admin.mahasiswa.edit',['student' => $student]);
         } catch (\Throwable $th) {
             return back()->with('error', 'Opps, Something was wrong!');
@@ -33,50 +34,52 @@ class MahasiswaController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'role_id' => 'required',
+            'name' => 'required',
+            'nim' => 'required',
+            'study_program' => 'required',
+            'password' => 'required',
+            'phone' => 'required',
+            'class' => 'required',
+            'gender' => 'required',
+            'email' => 'required',
+        ]);
         try {
-            $request->validate([
-                'nama' => 'required',
-                'nim' => 'required',
-                'prodi' => 'required',
-                'fakultas' => 'required',
-            ]);
-    
-            Mahasiswa::create($request->all());
-    
-            return back()->with('success', 'Berhasil ditambah');
+            User::create($request->all());
+            return to_route('admin.mahasiswa.index')->with('success', 'Berhasil ditambah');
         } catch (\Throwable $th) {
+            dd($th);
             return back()->with('error', 'Opps, Something was wrong!');
         }
     }
 
     public function update(Request $request)
     {
+        $request->validate([
+            'role_id' => 'required',
+            'name' => 'required',
+            'nim' => 'required',
+            'study_program' => 'required',
+            // 'password' => 'required',
+            'phone' => 'required',
+            'class' => 'required',
+            'gender' => 'required',
+            'email' => 'required',
+        ]);
         try {
-            $request->validate([
-                'nama' => 'required',
-                'nim' => 'required',
-                'prodi' => 'required',
-                'fakultas' => 'required',
-            ]);
     
-            Mahasiswa::where('id',$request->id,)->update([
-                'nama' => $request->nama,
-                'nim' => $request->nim,
-                'prodi' => $request->prodi,
-                'fakultas' => $request->fakultas,
-            ]);
-    
+            User::where('id',$request->id)->update($request->except('_token'));
             return back()->with('success', 'Berhasil dirubah');
         } catch (\Throwable $th) {
-            dd($th);
-            // return back()->with('error', 'Opps, Something was wrong!');
+            return back()->with('error', 'Opps, Something was wrong!');
         }
     }
 
     public function destroy($id){
         try{
 
-            $mahasiswa = Mahasiswa::findOrFail($id);
+            $mahasiswa = User::findOrFail($id);
             $mahasiswa->delete();
             return back()->with('success', 'Berhasil dihapus');
         } catch (\Throwable $th) {
